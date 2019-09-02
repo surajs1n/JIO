ac50704a87fd48b4f6d682becb99cacb1dcaf036
Suraj Singh <suraj.cse.ism@gmail.com>
2019-09-02 17:09:09 +0530

    Timer POJO added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JIO
-Java project to capture the perfomance of Input/Output classes on different environment.
+
+JIO **(Java Input/Output)** is a java project to capture the performance of various Input/Output classes on different environment.
---
+++ src/main/java/io/pojo/Timer.java
@@ -0,0 +1,26 @@
+package io.pojo;
+
+public class Timer {
+    private long startTime;
+    private long endTime;
+
+    public Timer() {
+        this.startTime = this.endTime = 0;
+    }
+
+    public void setStartTime(final long startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(final long endTime) {
+        this.endTime = endTime;
+    }
+
+    public long getStartTime() {
+        return this.startTime;
+    }
+
+    public long getEndTime() {
+        return this.endTime;
+    }
+}
